8579f44869fd76f80f57f09ab594af7b1caaab4e
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-07-10 12:58:07 +0500

    new changes added

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,7 @@ void main() async {
   ErrorWidget.builder = (FlutterErrorDetails details) {
     return CustomErrorWidget(errorDetails: details);
   };
-  
 
-
-
-
-
-
-
-
-  
   // Initialize Supabase
   // try {
   //   // SupabaseService();
